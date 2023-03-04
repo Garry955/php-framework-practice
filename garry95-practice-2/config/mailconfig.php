0127690cdcb31c61e9c405cec31e34b013d42700
Garry955 <124279810+Garry955@users.noreply.github.com>
2023-03-04 12:34:36 +0100

    Update mailconfig.php

--- garry95-practice-2/config/mailconfig.php
+++ garry95-practice-2/config/mailconfig.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    "host" => "mail.wawona.hu",
-    "auth" => true,
-    "username" => "project.test@wawona.hu",
-    "password" => "glq4a-n5ql+kgnq",
+    "host" => "",
+    "auth" => ,
+    "username" => "",
+    "password" => "",
     "port" => 465,
     "secure" => "ssl",
-    "email" => "info@wawona.hu",
-    "company" => "Wawona Internet Kft."
+    "email" => "",
+    "company" => ""
 ];
